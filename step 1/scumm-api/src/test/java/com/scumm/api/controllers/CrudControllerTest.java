c798ff77ab04ca8665df4012c06761ba7963c5fd
Isaac Palomero Bertran <isaac.palomero@linkeit.com>
2019-06-13 11:55:15 +0200

    refactor

--- step 1/scumm-api/src/test/java/com/scumm/api/controllers/CrudControllerTest.java
+++ step 1/scumm-api/src/test/java/com/scumm/api/controllers/CrudControllerTest.java
@@ -18,7 +18,7 @@ import java.util.Optional;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
-public class CategoryControllerTest {
+public class CrudControllerTest {
 
 
     private CategoryRepository categoryRepository;
