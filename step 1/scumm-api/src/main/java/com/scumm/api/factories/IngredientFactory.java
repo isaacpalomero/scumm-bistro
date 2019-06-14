125f43598fe371fe244d3a80ef5c13b99e2fac42
Sergio Bermúdez <sbermudezlozano@gmail.com>
2019-06-14 13:41:48 +0200

    Fixed bug: IngredientFactory

--- step 1/scumm-api/src/main/java/com/scumm/api/factories/IngredientFactory.java
+++ step 1/scumm-api/src/main/java/com/scumm/api/factories/IngredientFactory.java
@@ -1,13 +1,18 @@
 package com.scumm.api.factories;
 
+import com.scumm.api.contracts.CategoryContract;
 import com.scumm.api.contracts.IngredientContract;
+import com.scumm.core.domain.entities.Category;
 import com.scumm.core.domain.entities.Ingredient;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class IngredientFactory implements IIngredientFactory {
-    @Override
-    public Ingredient createFromContract(IngredientContract contract) {
-        return null;
+public class IngredientFactory extends AbstractModelFactory<Ingredient, IngredientContract>  implements IIngredientFactory {
+
+    @Autowired
+    public IngredientFactory(ModelMapper mapper) {
+        super(mapper, Ingredient.class);
     }
 }
