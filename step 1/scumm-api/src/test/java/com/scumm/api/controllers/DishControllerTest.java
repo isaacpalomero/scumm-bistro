96e366c250939ec1f9a1e71a3b1d03c4444c4f97
Maria Angeles Izquierdo Fernandez <mangeles.izquierdo@linkeit.com>
2019-06-14 12:26:37 +0200

    Improving dishcontroller

--- step 1/scumm-api/src/test/java/com/scumm/api/controllers/DishControllerTest.java
+++ step 1/scumm-api/src/test/java/com/scumm/api/controllers/DishControllerTest.java
@@ -3,13 +3,14 @@ package com.scumm.api.controllers;
 import com.scumm.api.contracts.DishContract;
 import com.scumm.api.contracts.DishIngredientContract;
 import com.scumm.api.contracts.IngredientContract;
+import com.scumm.api.exceptions.ModelNotFoundException;
 import com.scumm.api.factories.IDishFactory;
 import com.scumm.api.factories.IDishIngredientFactory;
 import com.scumm.api.factories.IIngredientFactory;
 import com.scumm.api.services.IDishService;
 import com.scumm.api.validators.IContractValidator;
 import com.scumm.core.domain.entities.Dish;
-import com.scumm.core.domain.entities.Ingredient;
+import com.scumm.core.domain.entities.Model;
 import com.scumm.core.domain.repositories.DishRepository;
 import org.bson.types.ObjectId;
 import org.junit.Assert;
@@ -27,7 +28,7 @@ import static org.mockito.Mockito.*;
 public class DishControllerTest {
 
     private DishController controller;
-    private final List<IngredientContract> ingredientList = new ArrayList<>();
+    private final List<DishIngredientContract> ingredientList = new ArrayList<>();
     private DishRepository repository;
     private ModelMapper modelMapper;
     private IDishFactory dishFactory;
@@ -39,10 +40,12 @@ public class DishControllerTest {
     private String dishId;
 
     private IDishService dishService;
+    private final Dish dish = new Dish();
 
 
     @Before
     public void setup() {
+        dishId = new ObjectId().toString();
         repository = mock(DishRepository.class);
         modelMapper = mock(ModelMapper.class);
         dishFactory = mock(IDishFactory.class);
@@ -51,14 +54,11 @@ public class DishControllerTest {
         dishService = mock(IDishService.class);
         controller = new DishController(repository, modelMapper, dishFactory, contractValidator, dishIngredientFactory, dishService);
 
-        //TODO: change ingredientContract por dishIngredientContract
-        ingredientContract.setId("alksjdflajfi3322212m1k12k2");
-        ingredientContract.setName("Quinoa");
-        ingredientFactory = mock(IIngredientFactory.class);
-        ingredientFactory.createFromContract(ingredientContract);
-        ingredientList.add(ingredientContract);
+        String ingredientId = "quinoa123455";
+        dishIngredientContract.setIngredientId(ingredientId);
+        dishIngredientContract.setQuantity(1);
+        ingredientList.add(dishIngredientContract);
 
-        dishId = "lkasl231lkadsjlka5454dsjlkads";
 
     }
 
@@ -66,38 +66,48 @@ public class DishControllerTest {
     //TODO => do we want to validate duplicate ingredients in list?
 
     @Test
-    public void addIngredientsSuccess() {
-        //expectations
+    public void addIngredientsSuccess() throws ModelNotFoundException {
+
+        when(dishFactory.getById(dishId)).thenReturn(dish);
+
+        // test
+        ResponseEntity response = controller.addIngredients(dishId, ingredientList);
 
         // verifications
-        verify(controller, times(1)).addIngredients(any(String.class), List<DishIngredientContract>.class);
-
-        //test
-        ResponseEntity response = controller.addIngredients(dishId, ingredientList);
+        verify(dishFactory, times(1)).getById(dishId);
+        verify(dishIngredientFactory, times(1)).createDishIngredientsFromContracts(ingredientList);
+        verify(dishService, times(1)).addIngredients(any(Dish.class), any(List.class));
         Assert.assertNull(response.getBody());
         Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
-
-        //TODO:
-        // NO - Verify dish exists in repository
-        // NO - Verify each ingredient exists in repository
-        // NO - Verify repository.save() is called
-        // Assert HTTP_CODE is CREATED/MODIFIED/TO_BE_DECIDED
-        // RESULT: Create in controller => controller.addIngredients(ingredientList)
     }
 
     @Test
-    public void addIngredientsFailDishDoesNotExist() {
-        //TODO:
-        // Assert dish does not exist
-        // Assert HTTP_CODE == NOT_FOUND
+    public void addIngredientsFailDishDoesNotExist() throws ModelNotFoundException {
+
+        // test
+        ResponseEntity response = controller.addIngredients(dishId, ingredientList);
+
+        verify(dishFactory, times(1)).getById(dishId);
+        verify(dishIngredientFactory, never()).createDishIngredientsFromContracts(ingredientList);
+        verify(dishService, never()).addIngredients(any(Dish.class), any(List.class));
+        Assert.assertNull(response.getBody());
+        Assert.assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
     }
 
-    @Test
-    public void addIngredientsFailIngredientDoesNotExist() {
-        //TODO:
-        // Verify dish exists in repository
-        // Assert ingredient does not exist
-        // Assert HTTP_CODE == NOT_FOUND
+   //@Test
+    public void addIngredientsFailIngredientDoesNotExist() throws ModelNotFoundException {
+
+        when(dishFactory.getById(dishId)).thenReturn(dish);
+
+        // test
+        ResponseEntity response = controller.addIngredients(dishId, ingredientList);
+
+        // verifications
+        verify(dishFactory, times(1)).getById(dishId);
+        verify(dishIngredientFactory, times(1)).createDishIngredientsFromContracts(ingredientList);
+        verify(dishService, times(1)).addIngredients(any(Dish.class), any(List.class));
+        Assert.assertNull(response.getBody());
+        Assert.assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
     }
 
 }
