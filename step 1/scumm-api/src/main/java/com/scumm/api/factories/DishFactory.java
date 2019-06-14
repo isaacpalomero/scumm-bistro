beff5b7908ede11d452bacc3ad5c1ae387d0f156
Isaac Palomero Bertran <isaac.palomero@linkeit.com>
2019-06-14 12:24:14 +0200

    DishFactory: getById() DishFactoryTest: getByIdSucces() and getByIdError()

--- step 1/scumm-api/src/main/java/com/scumm/api/factories/DishFactory.java
+++ step 1/scumm-api/src/main/java/com/scumm/api/factories/DishFactory.java
@@ -3,20 +3,34 @@ package com.scumm.api.factories;
 import com.scumm.api.contracts.DishContract;
 import com.scumm.api.exceptions.ModelNotFoundException;
 import com.scumm.core.domain.entities.Dish;
+import com.scumm.core.domain.repositories.DishRepository;
+import org.bson.types.ObjectId;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Component
 public class DishFactory extends AbstractModelFactory<Dish, DishContract> implements IDishFactory {
 
+    protected DishRepository dishRepository;
+
     @Autowired
-    public DishFactory(ModelMapper mapper) {
+    public DishFactory(ModelMapper mapper, DishRepository dishRepository) {
         super(mapper, Dish.class);
+        this.dishRepository = dishRepository;
     }
 
     @Override
     public Dish getById(String id) throws ModelNotFoundException {
-        return null;
+        ObjectId objectId = new ObjectId(id);
+        Optional<Dish> optionalDish = dishRepository.findById(objectId);
+
+        if(optionalDish.isPresent()) {
+            return optionalDish.get();
+        }
+        throw new ModelNotFoundException();
     }
 }
