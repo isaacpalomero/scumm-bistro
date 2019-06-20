a8e03c02fbe6ce7e82d875efc1f466f0e155d8a5
Sergio Bermúdez <sbermudezlozano@gmail.com>
2019-06-20 13:55:28 +0200

    Docker compose

--- step 1/scumm-bdd-test/src/test/java/com/scumm/bdd/adminpanel/AdminPanelSteps.java
+++ step 1/scumm-bdd-test/src/test/java/com/scumm/bdd/adminpanel/AdminPanelSteps.java
@@ -1,4 +1,28 @@
 package com.scumm.bdd.adminpanel;
 
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class AdminPanelSteps {
+
+    private WebDriver driver;
+
+    @Given("Un navegador")
+    public void un_navegador(){
+        driver = new FirefoxDriver();
+    }
+
+    @When("Entro en el panel de administracion")
+    public void abro_el_admin_panel(){
+        driver.get("http://localhost:8081");
+    }
+
+    @Then("Se abre el dashboard")
+    public void se_abre_el_dashboard(){
+        //driver.findElement(By.id(""))
+    }
 }
---
+++ step 1/scumm-bdd-test/src/test/resources/admin_panel/open_admin_panel.feature
@@ -0,0 +1,6 @@
+Feature: Abrir el admin panel
+
+  Scenario: Como restaurador quiero poder entrar en el panel de administracion
+    Given Un navegador
+    When Entro en el panel de administracion
+    Then Se abre el dashboard
