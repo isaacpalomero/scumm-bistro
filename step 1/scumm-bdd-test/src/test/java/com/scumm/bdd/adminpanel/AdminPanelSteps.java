f5b5b81d0eb36be7f39232cd8aaa6022c9b9523d
Sergio Bermúdez <sbermudezlozano@gmail.com>
2019-06-26 13:00:24 +0200

    Bdd Testing Selenium

--- step 1/scumm-bdd-test/src/test/java/com/scumm/bdd/adminpanel/AdminPanelSteps.java
+++ step 1/scumm-bdd-test/src/test/java/com/scumm/bdd/adminpanel/AdminPanelSteps.java
@@ -1,12 +1,14 @@
 package com.scumm.bdd.adminpanel;
 
+import cucumber.api.java.After;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class AdminPanelSteps {
 
@@ -23,7 +25,19 @@ public class AdminPanelSteps {
     }
 
     @Then("Se abre el dashboard")
-    public void se_abre_el_dashboard(){
-        //driver.findElement(By.id(""))
+    public void se_abre_el_dashboard() throws InterruptedException {
+        Thread.sleep(1000);
+        WebElement element = driver.findElement(By.xpath("/html/body/div/div[2]/div[2]/nav/div/a"));
+        Assert.assertNotNull(element);
+        Assert.assertEquals("DASHBOARD", element.getText());
+
+        WebElement linkTypography = driver.findElement(By.xpath("//*[@id=\"style-3\"]/ul[2]/li[4]/a"));
+        Assert.assertNotNull(linkTypography);
+        linkTypography.click();
+    }
+
+    @After
+    public void cerrar_browser(){
+        driver.close();
     }
 }
